442506902a034d60853cbfaf0c7456e842ce23c1
kunal-gitt <kunalthakur2023@gmail.com>
2022-10-14 16:29:16 +0530

    added getelementbyclassname

--- Dom.js
+++ Dom.js
@@ -32,6 +32,20 @@ add.style.fontWeight = "bold";
 add.style.color = "green";
 
 
-// search for example i am pushing this file
+//Get element by class name
+
+let  items = document.getElementsByClassName("list-group-item");
+
+console.log(items);
 
 
+console.log(items[1]);
+
+items[1].textContent = "Hello 2";
+items[1].style.fontWeight = "bold";
+items[1].style.backgroundColor = "yellow";
+
+for(let i = 0 ; i < items.length ; i++){
+
+    items[i].style.backgroundColor = "#f4f4f4";
+}
