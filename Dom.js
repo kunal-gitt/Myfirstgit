fe3d7cebd3af5f1d7cba70ece2b67a4f95ca442b
kunal-gitt <kunalthakur2023@gmail.com>
2022-10-14 17:12:14 +0530

    added queryselector

--- Dom.js
+++ Dom.js
@@ -1,35 +1,37 @@
 
-// console.dir(document);
-// console.log(document.URL);
-// console.log(document.title);
+console.dir(document);
+console.log(document.URL);
+console.log(document.title);
 
 
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+
+// Get element by Id
 
 
-// let headerTitle = document.getElementById("header-title");
-// let header = document.getElementById("main-header");
-// console.log(headerTitle);
+let headerTitle = document.getElementById("header-title");
+let header = document.getElementById("main-header");
+console.log(headerTitle);
 
 //difference bet textcontent and innertext 
-// innertext pays attention to the style as well
+//innertext pays attention to the style as well
 
-// console.log(headerTitle.textContent);
-// console.log(headerTitle.innerText);
+console.log(headerTitle.textContent);
+console.log(headerTitle.innerText);
 
-// headerTitle.innerHTML = "<h3>Hello</h3>";
+headerTitle.innerHTML = "<h3>Hello</h3>";
 
-// header.style.borderBottom = "solid 3px #000";
+header.style.borderBottom = "solid 3px #000";
 
-// let add = document.querySelector(".title");
+let add = document.querySelector(".title");
 
-// add.style.fontWeight = "bold";
-// add.style.color = "green";
+add.style.fontWeight = "bold";
+add.style.color = "green";
 
 
 //Get element by class name
@@ -53,7 +55,7 @@ for(let i = 0 ; i < items.length ; i++){
 
 items[2].style.backgroundColor = "green";
 
-// get element by Tag name
+// Get element by Tag name
 
 
 let  li = document.getElementsByTagName("li");
@@ -74,4 +76,48 @@ for(let i = 0 ; i < li.length ; i++){
 }
 
 li[2].style.backgroundColor = "green";
+
+
+//Query selector
+
+let headers = document.querySelector("#main-header");
+headers.style.borderBottom = "solid 4px #ccc";
+
+let input = document.querySelector("input");
+input.value = "hello world";
+
+
+let submit = document.querySelector("input[type='submit']");
+
+submit.value = "SEND";
+
+let item = document.querySelector(".list-group-item");
+item.style.color = "red";
+
+let lastchild = document.querySelector(".list-group-item:last-child");
+lastchild.style.color = "Blue";
+
+let secondchild = document.querySelector(".list-group-item:nth-child(2)");
+secondchild.style.color = "pink";
+
+
+//Query selector all
+
+let titles = document.querySelectorAll(".title");
+
+console.log(titles);
+titles[0].textContent = "Hello";
+
+
+let odd = document.querySelectorAll("li:nth-child(odd)");
+let even = document.querySelectorAll("li:nth-child(even)");
+
+for(let i = 0 ; i < odd.length ; i++){
+
+    odd[i].style.backgroundColor = "#f4f4f4";
+    even[i].style.backgroundColor = "#ccc";
+}
+
+
+
 
