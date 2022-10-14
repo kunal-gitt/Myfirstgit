8fd714415975102bfc910dc04d8abd81610d500d
kunal-gitt <kunalthakur2023@gmail.com>
2022-10-14 16:41:24 +0530

    Addded with tagname

--- Dom.js
+++ Dom.js
@@ -1,35 +1,35 @@
 
-console.dir(document);
-console.log(document.URL);
-console.log(document.title);
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
 
 
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.forms[0]);
-console.log(document.links);
-console.log(document.images);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
 
 
-let headerTitle = document.getElementById("header-title");
-let header = document.getElementById("main-header");
-console.log(headerTitle);
+// let headerTitle = document.getElementById("header-title");
+// let header = document.getElementById("main-header");
+// console.log(headerTitle);
 
 //difference bet textcontent and innertext 
 // innertext pays attention to the style as well
 
-console.log(headerTitle.textContent);
-console.log(headerTitle.innerText);
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
 
-headerTitle.innerHTML = "<h3>Hello</h3>";
+// headerTitle.innerHTML = "<h3>Hello</h3>";
 
-header.style.borderBottom = "solid 3px #000";
+// header.style.borderBottom = "solid 3px #000";
 
-let add = document.querySelector(".title");
+// let add = document.querySelector(".title");
 
-add.style.fontWeight = "bold";
-add.style.color = "green";
+// add.style.fontWeight = "bold";
+// add.style.color = "green";
 
 
 //Get element by class name
@@ -52,4 +52,26 @@ for(let i = 0 ; i < items.length ; i++){
 }
 
 items[2].style.backgroundColor = "green";
+
+// get element by Tag name
+
+
+let  li = document.getElementsByTagName("li");
+
+console.log(li);
+
+
+console.log(li[1]);
+
+li[1].textContent = "Hello 2";
+li[1].style.fontWeight = "bold";
+li[1].style.backgroundColor = "yellow";
+
+for(let i = 0 ; i < li.length ; i++){
+
+    li[i].style.backgroundColor = "#f4f4f4";
+    li[i].style.fontWeight = "bold";
+}
+
+li[2].style.backgroundColor = "green";
 
