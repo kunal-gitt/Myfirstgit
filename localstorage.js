3f089f927d19529fb6b3ac7263901b57a3dd4c70
kunal-gitt <kunalthakur2023@gmail.com>
2022-10-17 21:12:08 +0530

    Error while deleting the name and email

--- localstorage.js
+++ localstorage.js
@@ -33,9 +33,14 @@ function onsubmit(e){
 
       let myobj_serial = JSON.stringify(userdetails);
 
+      console.log(myobj_serial);
+      console.log(userdetails.email);
+
       localStorage.setItem( userdetails.email, myobj_serial);
 
+      let s = localStorage.getItem(userdetails.email);
 
+      console.log(s);
       showNewUserOnScreen(userdetails);
 
 
@@ -43,11 +48,49 @@ function onsubmit(e){
       
        function showNewUserOnScreen(user){
 
+        
+
             let parentNode = document.getElementById("listOfUser");
-            let childhtml = `<li> ${user.username} - ${user.email} </li>`
+            let childhtml = `<li> ${user.username} - ${user.email}
+                            <button onClick= ()= ${deleteUser(`${user.email}`)}> Delete </button>
+                </li>`;
 
              parentNode.innerHTML = parentNode.innerHTML + childhtml;
+
+
+            
        }
+
+
+         function deleteUser(emailId){
+
+                
+                localStorage.removeItem(emailId);
+         }
+
+          
+
+       
+
+
+         
+       //   if(localStorage.getItem(user.email) !== null ){
+
+            //        removeUserFromScreen(user.email);
+            //   }
+
+    //    function removeUserFromScreen(emailId){
+
+    //     let parentNode = document.getElementById("listOfUser");
+    //     let childNodeToBeDeleted = document.getElementById(emailId);
+
+    //       if(childNodeToBeDeleted){
+
+    //         parentNode.removeChild(childNodeToBeDeleted);
+    //       }
+            
+    //    }
+         
 
        
        
